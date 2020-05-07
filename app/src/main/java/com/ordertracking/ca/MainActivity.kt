5984022c081f8eadf9658750b07842e806938029
Gilson <gilsonjuniorpro@gmail.com>
2020-05-07 16:30:43 -0400

    This is just a simple UI to handle order tracking

---
+++ app/src/main/java/com/ordertracking/ca/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ordertracking.ca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
